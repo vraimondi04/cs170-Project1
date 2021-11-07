ccc2efd691b1e1ae44daf649673340da94350aeb
vraimondi04 <42946121+vraimondi04@users.noreply.github.com>
2021-11-07 03:45:14 -0800

    Update main.cpp
    added additional output for user

--- main.cpp
+++ main.cpp
@@ -15,12 +15,19 @@ int main() {
 
   Puzzle p;
   int userChoice = 0;
+  int userAlgo = 0;
 
   cout << "Welcome to my 8-puzzle solver!" << endl;
   cout << "Enter 0 to use the default puzzle, and enter 1 to create your own:" << endl;
   cin >> userChoice;
 
   p.makePuzzle(userChoice);
+
+  cout << "Please select your choice of algorithm:" << endl;
+  cout << "1. Uniform Cost Search / 2. A* with Misplaced Tile Heuristic / 3. A* with Manhattan Distance Heuristic" << endl;
+  cin >> userAlgo;
+
+  p.selectAlgorithm(userAlgo);
 
   return 0;
 }
