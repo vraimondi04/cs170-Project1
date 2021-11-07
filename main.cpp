52e51d70b4351c8b03ecac34ca1fe1652b47e9c4
vraimondi04 <42946121+vraimondi04@users.noreply.github.com>
2021-11-07 01:25:52 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,20 @@
 
 #include <iostream>
 #include <queue>
+#include "puzzle.hpp"
 
-using namespace std;
+using namespace std; 
 
 int main() {
-    
-    return 0;
+
+  Puzzle p;
+  int userChoice = 0;
+
+  cout << "Welcome to my 8-puzzle solver!" << endl;
+  cout << "Enter 0 to use the default puzzle, and enter 1 to create your own:" << endl;
+  cin >> userChoice;
+
+  p.makePuzzle(userChoice);
+
+  return 0;
 }
